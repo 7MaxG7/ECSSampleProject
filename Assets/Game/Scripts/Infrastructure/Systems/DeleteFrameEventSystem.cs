e306a90000bdef56fc181de2be2d17ab84122283
Maksim Girii <m.girii@cybertemple.one>
2026-05-14 16:07:12 +0200

    fix: missed scripts

---
+++ Assets/Game/Scripts/Infrastructure/Systems/DeleteFrameEventSystem.cs
@@ -0,0 +1,19 @@
+using Leopotam.EcsLite;
+
+namespace Infrastructure
+{
+    public abstract class DeleteFrameEventSystem : IEcsPostRunSystem
+    {
+        private readonly FrameComponentsService _frameComponentsService;
+
+        protected DeleteFrameEventSystem(FrameComponentsService frameComponentsService)
+        {
+            _frameComponentsService = frameComponentsService;
+        }
+
+        public void PostRun(IEcsSystems systems)
+        {
+            _frameComponentsService.ClearFrameComponents();
+        }
+    }
+}
