0c7a0d93a4bdec7c9578875415aef77eb5f0e58b
Maksim Girii <m.girii@cybertemple.one>
2026-08-21 09:39:34 +0200

    - upd: field rename

--- Assets/Game/Scripts/Infrastructure/Services/Locators/StaticDataService.cs
+++ Assets/Game/Scripts/Infrastructure/Services/Locators/StaticDataService.cs
@@ -8,23 +8,23 @@ namespace Infrastructure
 {
     public class StaticDataService
     {
-        private Dictionary<string, UnitConfig> _unitDatas;
+        private Dictionary<string, UnitConfig> _unitConfigs;
 
         public void Init()
         {
-            _unitDatas = Resources
+            _unitConfigs = Resources
                 .LoadAll<UnitConfig>(Constants.UNIT_DATA_PATH)
                 .Where(data => data.IsEnabled)
                 .ToDictionary(data => data.Id, data => data);
         }
 
         public UnitConfig GetUnit(string id)
-            => TryGetData(_unitDatas, id);
+            => GetData(_unitConfigs, id);
 
         public UnitConfig GetAnyUnit(UnitSpecialization specialization)
-            => _unitDatas.Values.FirstOrDefault(data => data.Specialization.Equals(specialization));
+            => _unitConfigs.Values.FirstOrDefault(data => data.Specialization.Equals(specialization));
 
-        private TConfig TryGetData<TId, TConfig>(IReadOnlyDictionary<TId, TConfig> datas, TId id)
+        private TConfig GetData<TId, TConfig>(IReadOnlyDictionary<TId, TConfig> datas, TId id)
             => datas.GetValueOrDefault(id);
     }
 }
